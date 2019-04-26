93ff1f2fbad019597a654e55cf677a6a005cd54d
fabio <fbio.alves095@gmail.com>
2019-04-25 21:23:54 -0300

    Deu um problema inesperado na interface, aparentemente corrigi.

--- computacao_grafica/src/entrega2/Main.java
+++ computacao_grafica/src/entrega2/Main.java
@@ -3,7 +3,6 @@ package entrega2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
